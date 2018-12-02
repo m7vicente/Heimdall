ceed1048906bc2e6aba6e17f8a379c0208deeefe
pazthiago1 <pazthiagolima@gmail.com>
2018-12-01 22:10:07 -0200

    Logando com o usuário na aplicacao java

--- Heimdall_API/heimdall/src/main/java/heimdall/main.java
+++ Heimdall_API/heimdall/src/main/java/heimdall/main.java
@@ -22,11 +22,11 @@ public class main {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		 Usuario user = new Usuario("mgvicente@gmail.com","rola");
-		//Usuario user = new Usuario("", "");
+		 //Usuario user = new Usuario("mgvicente@gmail.com","rola");
+		Usuario user = new Usuario("", "");
 		CallService service = new CallService();
 
-		/*while (true) {
+		while (true) {
 			user.setEmail(JOptionPane.showInputDialog("Insira seu Email: "));
 
 			while (user.getEmail().equals("")) {
@@ -55,7 +55,7 @@ public class main {
 				break;
 			}
 
-		}*/
+		}
 
 		user = service.ObterUsuario(user);
 
