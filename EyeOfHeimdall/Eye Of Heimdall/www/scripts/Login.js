d6ad8df16082e6f2a4c02c2ff479e1f1878af4b3
Matheus Vicente <mvicente@outlook.com.br>
2018-12-05 14:28:59 -0200

    cors da classe de login alterado, tela de login criada

---
+++ EyeOfHeimdall/Eye Of Heimdall/www/scripts/Login.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+
+});
+
+var email = $('#txtEmail');
+var senha = $('#txtSenha');
+
+function user() {
+    this.codUsuario;
+    this.nomeCompleto;
+    this.email;
+    this.cargo;
+    this.senha;
+    this.ativo;
+}
+
+
+$('#btnLogar').click(function () {
+    usuario = new user();
+    usuario.email = email.val();
+    usuario.senha = senha.val();
+    login(usuario);
+});
+
+function login(user) {
+
+    var URL = "http://localhost:52121/api/Usuario/";
+    //var URL = "https://heimdallview.azurewebsites.net/api/Usuario/";
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": URL,
+        "method": "POST",
+        "data": user,
+        "Content-Type": "application/json"        
+    };
+    $.ajax(settings).done(function (response) {
+        alert(response.d);
+    });
+}
